c4ae0d28f19b1f37ae5ac096fb74243746473fb0
Michel Buczynski <boutch@gmail.com>
2017-06-18 16:49:10 -0400

    Adding batch for start Xserver , and Xterm ....

--- start_CygWin64_X_terminal.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-c:\tools\cmdow /run /hid c:\tools\cygwin\bin\bash.exe --login -c "DISPLAY=:0 /bin/konsole >/dev/null 2>&1 &"
-c:\tools\cmdow @ /cls
--- start_WSL_X_terminal.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-c:\tools\cmdow @ /hid
-start /b outbash ~ -c "export DISPLAY=:0 ; xfce4-terminal &"
-c:\tools\cmdow @ /cls
